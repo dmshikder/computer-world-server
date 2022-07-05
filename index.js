c534af63aedc75318b2e60817419ec520a9920b2
dmshikder <dmshikder@gmail.com>
2022-07-06 05:34:54 +0600

    heroku created

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ app.use(cors());
 app.use(express.json());
 
 
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+            return res.status(403).send({message:'Forbidden access'})
+        }
+        console.log('decoded', decoded)
+    })
+   
+        
+        next();
+
+}
+
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wyqlabu.mongodb.net/?retryWrites=true&w=majority`;
@@ -66,7 +85,8 @@ app.delete('/inventory/:id', async(req,res)=>{
 
 // my items  api 
 
-    app.get('/myItem', async(req,res)=>{
+    app.get('/myItem', verifyJWT, async(req,res)=>{
+        
         const email = req.query.email;
         const query ={email};
         const cursor = myItemCollection.find(query);
