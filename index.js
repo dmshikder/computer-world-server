5dd6cf1e6400e43e7d07071585b0362e720ac222
dmshikder <dmshikder@gmail.com>
2022-07-05 07:00:51 +0600

    add new product completed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
@@ -15,6 +15,38 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wyqlabu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        await client.connect();
+        const inventoryCollection = client.db('computer-world').collection('product');
+        app.get('/inventory', async(req,res)=>{
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+app.get('/inventory/:id', async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id: ObjectId(id)};
+    const product = await inventoryCollection.findOne(query);
+    res.send(product);
+});
+
+// post 
+app.post('/inventory', async(req,res)=>{
+    const newProduct = req.body;
+    const result = await inventoryCollection.insertOne(newProduct);
+    res.send(result);
+})
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir);
 
 
 
