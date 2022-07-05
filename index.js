22ffa8dff0702b4dace096c7be5920b59802d75f
dmshikder <dmshikder@gmail.com>
2022-07-05 16:26:39 +0600

    delete api created

--- index.js
+++ index.js
@@ -38,7 +38,15 @@ app.post('/inventory', async(req,res)=>{
     const newProduct = req.body;
     const result = await inventoryCollection.insertOne(newProduct);
     res.send(result);
-})
+});
+
+// delete 
+app.delete('/inventory/:id', async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id: ObjectId(id)};
+    const result = await inventoryCollection.deleteOne(query);
+    res.send(result);
+});
 
     }
     finally{
