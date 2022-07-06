7e15ac8f40e871973eb423ed34a36a2e1158b501
dmshikder <dmshikder@gmail.com>
2022-07-07 02:10:51 +0600

    my item delete api created

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ async function run() {
     app.delete("/myItem/:id", async (req, res) => {
         const id = req.params.id;
         const query = { _id: ObjectId(id) };
-        const result = await inventoryCollection.deleteOne(query);
+        const result = await myItemCollection.deleteOne(query);
         res.send(result);
       });
 
