7156613b8ac6902c4eb92ea1802a3eb62acb6b1f
dmshikder <dmshikder@gmail.com>
2022-07-07 00:59:59 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors = require('cors');
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,124 +10,108 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-function verifyJWT(req, res, next){
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
-        return res.status(401).send({message: 'unauthorized access'})
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
     }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
-        if(err){
-            return res.status(403).send({message:'Forbidden access'})
-        }
-        console.log('decoded', decoded)
-    })
-   
-        
-        next();
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+    next();
+  });
 
+  
 }
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wyqlabu.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-async function run(){
-    try{
-        await client.connect();
-        const inventoryCollection = client.db('computer-world').collection('product');
-
-        const myItemCollection = client.db('computer-world').collection('myItem');
-
-        // auth
-        app.post('/login', async(req,res)=>{
-            const user = req.body;
-            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn: "1d"
-            });
-            res.send({accessToken});
-        })
-
-        app.get('/inventory', async(req,res)=>{
-            const query = {};
-            const cursor = inventoryCollection.find(query);
-            const products = await cursor.toArray();
-            res.send(products);
-        });
-
-app.get('/inventory/:id', async(req,res)=>{
-    const id = req.params.id;
-    const query = {_id: ObjectId(id)};
-    const product = await inventoryCollection.findOne(query);
-    res.send(product);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
-// post 
-app.post('/inventory', async(req,res)=>{
+async function run() {
+  try {
+    await client.connect();
+    const inventoryCollection = client
+      .db("computer-world")
+      .collection("product");
 
-   
+    const myItemCollection = client.db("computer-world").collection("myItem");
 
-    const newProduct = req.body;
-    const result = await inventoryCollection.insertOne(newProduct);
-    res.send(result);
-});
+    // auth
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
 
-// delete 
-app.delete('/inventory/:id', async(req,res)=>{
-    const id = req.params.id;
-    const query = {_id: ObjectId(id)};
-    const result = await inventoryCollection.deleteOne(query);
-    res.send(result);
-});
+    app.get("/inventory", async (req, res) => {
+      const query = {};
+      const cursor = inventoryCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
 
-// my items  api 
+    app.get("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await inventoryCollection.findOne(query);
+      res.send(product);
+    });
 
-    app.get('/myItem', verifyJWT, async(req,res)=>{
-        
-        const email = req.query.email;
-        const query ={email};
+    // post
+    app.post("/inventory", async (req, res) => {
+      const newProduct = req.body;
+      const result = await inventoryCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    // delete
+    app.delete("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await inventoryCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // my items  api
+
+    app.get("/myItem", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const query = { email: email };
         const cursor = myItemCollection.find(query);
         const items = await cursor.toArray();
         res.send(items);
-    })
-
-
-
-
-
-    app.post('/myItem', async(req,res) =>{
-        const item = req.body;
-        const result = await myItemCollection.insertOne(item);
-        res.send(result);
+      }
+      else{
+        res.status(403).send({message: 'forbidden access'})
+      }
     });
 
-// app.get('/inventory', async(req,res)=>{
-//     const email = req.query.email;
-//     console.log(email);
-//     const query = {email:email};
-   
-//     const cursor = inventoryCollection.find(query);
-//     const myItems = await cursor.toArray();
-//     res.send(myItems);
-// })
-
-    }
-    finally{
-
-    }
-
+    app.post("/myItem", async (req, res) => {
+      const item = req.body;
+      const result = await myItemCollection.insertOne(item);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req,res)=>{
-    res.send('running server')
+app.get("/", (req, res) => {
+  res.send("running server");
 });
 
-app.listen(port, ()=>{
-    console.log('listening', port)
-})
+app.listen(port, () => {
+  console.log("listening", port);
+});
